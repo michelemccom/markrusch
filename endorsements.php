7e8dadafe1596d81796e6381d7683741e1e943e6
michelemccom <michele_martin@mccom.com>
2016-02-02 08:54:42 -0600

    fixing columns

--- endorsements.php
+++ endorsements.php
@@ -13,16 +13,16 @@
 					<li>Mayor of McKinney<br />Brian Loughmiller</li>
 					<li>Mayor of Allen<br /> Steve Terrell</li>
 					<li>McKinney Mayor Pro Tem<br/> Travis Ussery</li>
-
+					<li></li>
 					<li>Constable Precinct 3 Sammy Knapp</li>
 					<li>Collin County Commissioner<br />Duncan Webb	</li>
 					<li> Collin County Commissioner<br />Cheryl Williams</li>
 					<li>Patriot Texas Conservative Activists</li>
-					
 					<li>Collin County Deputies Association</li>
 					<li> McKinney Police Association</li>
 					<li>Plano Police Association</li>
 					<li>Farmersville Police Employees Association </li>
+			
 
 				</ul>
 				<ul>
